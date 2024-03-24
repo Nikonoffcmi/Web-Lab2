59c98ad2d7f58ff232bb2138584f93324a16b557
Nikonoff <nikchumadev@gmail.com>
2024-03-24 12:55:46 +0700

    Slider change

--- Image-Slider/main.js
+++ Image-Slider/main.js
@@ -1,22 +1,13 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
     loop: true,
-  
-    // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: ".swiper-pagination",
+        clickable: true,
     },
-  
-    // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
+});
